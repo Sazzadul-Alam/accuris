1d91d00c0833a86d254a34bbb755cd688c8812d1
Intesar <stevejobsone@Intesars-Mac-Mini.local>
2025-11-19 17:32:56 +0600

    made all signup pages and forms responsive

--- src/app/features/auth/pages/signup/signup-user-consent-form/signup-user-consent-form.component.ts
+++ src/app/features/auth/pages/signup/signup-user-consent-form/signup-user-consent-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import { CommonModule } from '@angular/common';
 
 @Component({
