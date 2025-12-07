a113b6004810d9c9f1ee882859a6eeef40583348
kaungkon <mallickkaungkon@gmail.com>
2025-12-07 16:31:20 +0600

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SideBarComponent } from './dashboard/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
