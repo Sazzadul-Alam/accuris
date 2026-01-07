f161d8a3e7879bead58a85c678d0f017888627e5
kaungkon <mallickkaungkon@gmail.com>
2026-01-07 18:13:11 +0600

    initial

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -12,10 +12,22 @@ export class DashboardComponent {
     business_credit: {selected:false}
   };
   currentTable: string;
+  progressValue = 40;
+
+  radius = 30;
+  circumference = 2 * Math.PI * this.radius;
+
+  dashOffset = this.circumference;
 
   ngOnInit() {
     this.currentTable ='individual_credit';
     this.tables[this.currentTable].selected = true;
+    this.dashOffset = this.circumference - (this.progressValue / 100) * this.circumference;
+  }
+  get progressColor() {
+    if (this.progressValue < 40) return '#ef4444';
+    if (this.progressValue < 70) return '#f59e0b';
+    return '#22c55e';
   }
   openIndividualModal() {
     console.log('Opening individual modal');
